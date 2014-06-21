5d8f5dc663ca20cb2607d73cc48759e70373dcb4
enome <geert.pasteels@gmail.com>
2014-06-21 17:56:13 +0200

    komponant ready

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 /** @jsx React.DOM */
 
-import React from 'react';
-import suitcss from 'suitcss-mixin';
+import React from 'react/react.js';
+import suitcss from 'suitcss-mixin/index.js';
 
 var Button = React.createClass({
 
   mixins: [suitcss],
 
-  /* LOGIC */
-
-  /* LIFECYCLE EVENTS */
-
-  /* RENDER */
-
   render: function () {
     return this.transferPropsTo(
-      React.DOM.button({ className: this.classNames('Button') }, this.props.children)
+      <button className={this.classNames('Button')}>
+        {this.props.children}
+      </button>
     );
   },
 
